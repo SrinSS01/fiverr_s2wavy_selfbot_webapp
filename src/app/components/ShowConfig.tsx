a28b296750063db418eff48cf79f2576aab282b6
SrinSS01 <srinjoy.s.01.in@gmail.com>
2024-06-07 23:01:57 +0530

    forst commit

--- src/app/components/ShowConfig.tsx
+++ src/app/components/ShowConfig.tsx
@@ -1,12 +1,61 @@
 'use client'
 
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { SelfBotUsers } from "../page";
+import Close from "../icons/Close.svg"
+import Image from "next/image";
 
+type Server = {
+	id: string;
+	name: string;
+	icon?: string;
+};
 
-export default function ShowConfig({ user }: { user: SelfBotUsers; }) {
-    // const [] = useState()
-    return <div>
-        { user.name }
-    </div>
+export default function ShowConfig({ user, setShowConfig }: {
+	user: SelfBotUsers,
+	setShowConfig?: (value: ( ( (prevState: boolean) => boolean ) | boolean )) => void
+}) {
+	const [ servers, setServers ] = useState<Server[]>([])
+	const [ error, setError ] = useState<string | undefined>();
+	// const [ hoverIndex, setHoverIndex ] = useState<number>()
+	useEffect(() => {
+		fetch(`http://127.0.0.1:8090/servers/${ user.user_id }`)
+			.then(res => res.json())
+			.then(_json => setServers(_json))
+			.catch(console.error);
+	}, []);
+	return <div className={ "flex flex-col gap-5" }>
+		<Image className={ "fixed top-10 right-10 cursor-pointer" } src={ Close } alt={ "close" } onClick={ () => {
+			if ( setShowConfig ) {
+				setServers([]);
+				setShowConfig(false);
+			}
+		} }/>
+		<div className={ "flex flex-row gap-5" }>
+			<div className={ "flex-1 overflow-auto" }>
+				{ servers.map((server, index) =>
+					<div
+						className={ "flex gap-5 items-center relative hover:bg-gray-800 p-5" }
+						key={ index }
+						/*onMouseEnter={ () => {
+							setHoverIndex(index);
+						} }
+						onMouseLeave={ () => {
+							setHoverIndex(undefined)
+						} }*/>
+						<img
+							className={ "rounded-full" }
+							width={ 100 }
+							src={ server.icon !== null ? `https://cdn.discordapp.com/icons/${ server.id }/${ server.icon }.webp?size=256` : `https://cdn.discordapp.com/embed/avatars/0.png` }
+							alt={ server.name }/>
+						<div className={ "w-[200px]" }>
+							<div className={ "font-bold truncate" }>{ server.name }</div>
+							<div>{ server.id }</div>
+						</div>
+					</div>) }
+			</div>
+			<div className={ "flex-1 overflow-auto h-fit" }>Test</div>
+		</div>
+		<div className={ error ? "absolute bottom-0 left-0 bg-red-500 pl-4 pr-4 p-2" : "" }>{ error ?? "" }</div>
+	</div>
 }
--- src/app/components/TokenInput.tsx
+++ src/app/components/TokenInput.tsx
@@ -1,50 +1,64 @@
 'use client'
 
-import React, { useRef, useState } from 'react';
+import React, { Dispatch, SetStateAction, useRef, useState } from 'react';
+import { SelfBotUsers } from "@/app/page";
 
-export default function TokenInput() {
-  const tokenRef = useRef('');
-  const [error, setError] = useState<string | undefined>();
+interface TokenInputProps {
+	resetUsers?: (value: ( ( (prevState: SelfBotUsers[]) => SelfBotUsers[] ) | SelfBotUsers[] )) => void
+}
 
-  const handleAddClick = async () => {
-    if (tokenRef.current.trim().length === 0) {
-      return;
-    }
+export default function TokenInput({ resetUsers }: TokenInputProps) {
+	const [ token, setToken ] = useState("")
+	const [ error, setError ] = useState<string | undefined>();
 
-    try {
-      const response = await fetch('https://studious-broccoli-vr7j4qg6p492xppj-8090.app.github.dev/self_bot_users', {
-        method: 'POST',
-        body: JSON.stringify({
-          token: tokenRef.current,
-        }),
-        cache: 'no-store',
-      });
+	const handleAddClick = async () => {
+		if ( token.trim().length === 0 ) {
+			return;
+		}
 
-      if (!response.ok) {
-        const errorJson = await response.json();
-        setError(JSON.stringify(errorJson));
-        setTimeout(() => setError(undefined), 5000)
-      } else {
-        setError(undefined); // Clear any previous error
-      }
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      setError('An error occurred while fetching data.');
-    }
-  };
+		try {
+			const response = await fetch('http://127.0.0.1:8090/self_bot_users', {
+				method: 'POST',
+				body: JSON.stringify({
+					token: token,
+				}),
+				cache: 'no-store',
+			});
 
-  return (
-    <div className={"m-10 flex flex-col font-bold align-middle items-center"}>
-      <div className={"flex outline outline-2 w-fit"}>
-        <input    
-            className={"pl-2 pr-5 text-black"}
-            type="text"
-            placeholder="Enter Token"
-            onChange={(event) => (tokenRef.current = event.target.value)}
-        />
-        <input className={"p-2 pl-4 pr-4 bg-gray-800 hover:bg-gray-500 active:bg-gray-800"} type="button" value="add" onClick={handleAddClick} />
-      </div>
-      <div className={ error ? "absolute bottom-0 left-0 bg-red-500 pl-4 pr-4 p-2": ""}>{ error ?? "" }</div>
-    </div>
-  );
+			if ( !response.ok ) {
+				const errorJson = await response.json();
+				setError(JSON.stringify(errorJson));
+				setTimeout(() => setError(undefined), 5000)
+			} else {
+				setError(undefined); // Clear any previous error
+				if ( resetUsers ) {
+					fetch("http://127.0.0.1:8090/self_bot_users", { cache: "no-store" })
+						.then(res => res.json())
+						.then(_json => resetUsers(_json))
+						.catch(console.error);
+					console.log("reset users");
+				}
+			}
+			setToken("")
+		} catch ( error ) {
+			console.error('Error fetching data:', error);
+			setError('An error occurred while fetching data.');
+		}
+	};
+
+	return (
+		<div className={ "m-10 flex flex-col font-bold align-middle items-center" }>
+			<div className={ "flex outline outline-2 w-fit" }>
+				<input
+					className={ "pl-2 pr-5 text-black" }
+					type="text"
+					placeholder="Enter Token"
+					onChange={ (event) => setToken(event.target.value) }
+				/>
+				<input className={ "p-2 pl-4 pr-4 bg-gray-800 hover:bg-gray-500 active:bg-gray-800" } type="button"
+					   value="add" onClick={ handleAddClick }/>
+			</div>
+			<div className={ error ? "absolute bottom-0 left-0 bg-red-500 pl-4 pr-4 p-2" : "" }>{ error ?? "" }</div>
+		</div>
+	);
 }
